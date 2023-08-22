efce579fbda4d923fd82003243117672102864c1
Yermi <kycasdzxc@naver.com>
2023-08-22 15:32:49 +0900

    Iterator 패턴 : 처리를 반복한다
    컬렉션의 요소들의 기본 표현​(리스트, 스택, 트리 등)​을 노출하지 않고 그들을 하나씩 순회할 수 있으며, 무엇인가 많이 모여 있을 때 이를 순서대로 가리키며 전체를 검색하고 처리를 반복하는 패턴

---
+++ src/iterator/exam/BookShelf.java
@@ -0,0 +1,32 @@
+package iterator.exam;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class BookShelf implements Iterable<Book> {
+	// 배열이 아닌 ArrayList 사용하기
+	private List<Book> books;
+	
+	public BookShelf() {
+		this.books = new ArrayList<>();
+	}
+	
+	public Book getBookAt(int index) {
+		return books.get(index);
+	}
+	
+	public void appendBook(Book book) {
+		this.books.add(book);
+	}
+	
+	public int getLength() {
+		return books.size();
+	}
+	
+	@Override
+	public Iterator<Book> iterator() {
+		return new BookShelfIterator(this);
+	}
+	
+}
---
+++ src/iterator/exam/Main.java
@@ -0,0 +1,33 @@
+package iterator.exam;
+
+import java.util.Iterator;
+
+public class Main {
+	public static void main(String[] args) {
+		BookShelf bookShelf = new BookShelf();
+		bookShelf.appendBook(new Book("Around the World in 80 Days"));
+		bookShelf.appendBook(new Book("Bible"));
+		bookShelf.appendBook(new Book("Cinderella"));
+		bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+		
+		// 똑같은 값 한 번 더 추가
+		bookShelf.appendBook(new Book("Around the World in 80 Days"));
+		bookShelf.appendBook(new Book("Bible"));
+		bookShelf.appendBook(new Book("Cinderella"));
+		bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+		
+		// 명시적으로 Iterator를 사용하는 방법
+		Iterator<Book> it = bookShelf.iterator();
+		while(it.hasNext()) {
+			Book book = it.next();
+			System.out.println(book.getName());
+		}
+		System.out.println();
+		
+		// 확장 for문을 사용하는 방법
+		for(Book book : bookShelf) {
+			System.out.println(book.getName());
+		}
+		System.out.println();
+	}
+}
