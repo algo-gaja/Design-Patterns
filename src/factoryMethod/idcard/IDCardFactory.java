547867a7ac1c93d084d7cb6448fa94e0db5970e5
Yermi <kycasdzxc@naver.com>
2023-08-24 14:53:31 +0900

    Factory Method 패턴 : 하위 클래스에서 인스턴스를 만든다
    부모 클래스에서 객체들을 생성할 수 있는 인터페이스를 제공하지만, 자식 클래스들이 생성될 객체들의 유형을 변경할 수 있도록 하는 생성 패턴

---
+++ src/factoryMethod/idcard/IDCardFactory.java
@@ -0,0 +1,17 @@
+package factoryMethod.idcard;
+
+import factoryMethod.framework.Factory;
+import factoryMethod.framework.Product;
+
+public class IDCardFactory extends Factory {
+
+	@Override
+	protected Product createProduct(String owner) {
+		return new IDCard(owner);
+	}
+
+	@Override
+	protected void registerProduct(Product product) {
+		System.out.println(product + "을 등록했습니다.");
+	}
+}
